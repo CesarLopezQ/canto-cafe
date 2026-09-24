bd74a08acb989924e137ee83a3e81b4898b0109b
Cesar Quevedo <cesarlopez99@live.com>
2026-09-24 10:31:36 -0700

    Added reset.css import

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
 import Main from "@/Main/Main.jsx";
-("./reset.css");
+import "./reset.css";
 
 createRoot(document.getElementById("root")).render(<Main />);
